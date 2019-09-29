827f022f61521f6fced3072a911a8e49e4c146ec
Yogrishi Mulliah <yogrishi.mulliah1@umail.uom.ac.mu>
2019-09-29 17:01:22 +0400

    working refresh and exit

--- BatteryMonitor/MainWindow.xaml.cs
+++ BatteryMonitor/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -23,16 +24,48 @@ namespace BatteryMonitor
         public MainWindow()
         {
             InitializeComponent();
+            batteryStatus.Content = GetPowerSource();
+            powerSource.Content = GetBatteryStatus();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Application.Exit()
+            this.Close();
+            //Application.Exit();
         }
 
         private void RefreshBtn_Click(object sender, RoutedEventArgs e)
         {
+            batteryStatus.Content = GetPowerSource();
+            powerSource.Content = GetBatteryStatus();
+        }
 
+        public String GetPowerSource()
+        {
+
+            string strPowerLineStatus = "Default";
+            // Getting the current system power status.
+            switch (SystemInformation.PowerStatus.PowerLineStatus)
+            {
+                case System.Windows.Forms.PowerLineStatus.Offline:
+                    strPowerLineStatus = "Battery";
+                    break;
+                case System.Windows.Forms.PowerLineStatus.Online:
+                    strPowerLineStatus = "AC Power";
+                    break;
+                case System.Windows.Forms.PowerLineStatus.Unknown:
+                    strPowerLineStatus = "Unknown";
+                    break;
+            }
+            return strPowerLineStatus;
+        }
+        public float GetBatteryStatus()
+        {
+            float batterylife;
+            batterylife = SystemInformation.PowerStatus.BatteryLifePercent;
+            batterylife *= 100.0f;
+            return batterylife;
         }
     }
 }
+
